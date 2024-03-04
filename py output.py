17694cd685cf2dff12924664e0f82467126693c2
abigael2024 <161236853+abigael2024@users.noreply.github.com>
2024-03-04 22:28:38 +0300

    Add files via upload

---
+++ py output.py
@@ -0,0 +1,8 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\ADMIN\AppData\Local\Programs\Python\Python312\users input.py
+What is your name?abigael
+what is your age?18
+where do you live?kericho
+Helloabigael,you are18years old and live inkericho.
